e8835a8781c9c0ce5de68fde02c5269aec1488a9
sheying <1172878727@qq.com>
2026-05-21 19:39:35 +0800

    修复移动端底部导航滚动

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,7 +8,7 @@ const showTabbar = computed(() => route.meta.tabbar)
 
 <template>
   <router-view />
-  <van-tabbar v-if="showTabbar" route safe-area-inset-bottom>
+  <van-tabbar v-if="showTabbar" route fixed safe-area-inset-bottom>
     <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
     <van-tabbar-item to="/records" icon="orders-o">明细</van-tabbar-item>
     <van-tabbar-item to="/quick-add" icon="plus">记一笔</van-tabbar-item>
@@ -16,4 +16,3 @@ const showTabbar = computed(() => route.meta.tabbar)
     <van-tabbar-item to="/settings" icon="setting-o">设置</van-tabbar-item>
   </van-tabbar>
 </template>
-
